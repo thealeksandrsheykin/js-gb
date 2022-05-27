95536fad46792cab662e752dec785411bda7e59c
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-05-27 12:56:32 +0300

    Create exercise-2.js

---
+++ lesson-1/exercise-2/exercise-2.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env none
+
+/*
+    2. Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать
+       значение из name в admin. Вывести admin (должно вывестись «Василий»).
+*/
+
+let name = 'Василий';
+let admin = name;
+console.log(admin);
