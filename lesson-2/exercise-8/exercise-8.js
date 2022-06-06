e96c15680bc944719680050e886b62ecaeee7b96
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-06-06 15:21:01 +0300

    Exercise-8 Done!

--- lesson-2/exercise-8/exercise-8.js
+++ lesson-2/exercise-8/exercise-8.js
@@ -5,3 +5,12 @@
     power(val, pow), где val — заданное число, pow –— степень.
 */
 
+function power(val, pow) {
+    if (pow != 1) {
+        return val * power(val, pow - 1);
+    }
+    return val
+}
+console.log(power(2,3))
+
+console.log(Math.pow(2,3))
