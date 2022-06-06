5db264abab0301c20056257ffee18bae7435529e
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-06-06 15:15:48 +0300

    Exercise-6 Done!

--- lesson-2/exercise-6/exercise-6.js
+++ lesson-2/exercise-6/exercise-6.js
@@ -6,3 +6,20 @@
 операций (использовать функции из пункта 5) и вернуть полученное значение (применить switch).
 */
 
+function mathOperation(arg1,arg2,operation){
+    switch (operation){
+        case('sum'):
+           return sum(arg1,arg2)
+        case('dif'):
+            return dif(arg1,arg2)
+        case('mul'):
+            return mul(arg1,arg2)
+        case('div'):
+            return div(arg1,arg2)
+    }
+}
+
+console.log(mathOperation(1,2,'sum'))
+console.log(mathOperation(1,2,'dif'))
+console.log(mathOperation(1,2,'mul'))
+console.log(mathOperation(1,2,'div'))
