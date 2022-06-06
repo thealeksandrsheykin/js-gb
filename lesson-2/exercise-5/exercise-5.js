75e845be835b203c17670c37cc79555ac194a5e8
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-06-06 15:13:44 +0300

    Exercise-5 Done!

--- lesson-2/exercise-5/exercise-5.js
+++ lesson-2/exercise-5/exercise-5.js
@@ -5,3 +5,19 @@
    параметрами. Обязательно использовать оператор return.
 */
 
+function sum(x,y) {
+    return x + y
+}
+function dif(x,y) {
+    return x - y
+}
+function mul(x,y) {
+    return x * y
+}
+function div(x,y) {
+    return x / y
+}
+console.log(sum(1,2))
+console.log(dif(1,2))
+console.log(mul(1,2))
+console.log(div(1,2))
