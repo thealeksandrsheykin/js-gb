341cd1d1ab5d2fdc0f0ac732619c0863b3c2ad8b
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-06-08 13:06:59 +0300

    Exercise-2 Done!

--- lesson-3/exercise-2/exercise-2.js
+++ lesson-3/exercise-2/exercise-2.js
@@ -4,3 +4,18 @@
 2. С этого урока начинаем работать с функционалом интернет-магазина. Предположим, есть сущность корзины. Нужно реализовать
 функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
  */
+
+function cart_price_basket(client_basket, store_prices) {
+    let price_basket_client = 0;
+    for (let product of client_basket) {
+        price_basket_client += store_prices[product];
+    }
+    return price_basket_client
+}
+
+let store_prices  = {'Хлеб': 71.99, 'Молоко': 67.40, 'Мясо': 320, 'Рыба': 450.30};
+let client_basket = ['Хлеб', 'Молоко'];
+
+price_basket = cart_price_basket(client_basket,store_prices).toString().split('.')
+
+console.log(`Стоимость клиентской корзины: ${price_basket[0]} руб. ${price_basket[1]} коп.`)
