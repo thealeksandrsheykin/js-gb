694bfb74c3beb6e4b4cdfa8dbcae06c6eda840bc
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-06-08 12:38:01 +0300

    Exercise-1 Done!

--- lesson-3/exercise-1/exercise-1.js
+++ lesson-3/exercise-1/exercise-1.js
@@ -3,3 +3,14 @@
 /*
 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
 */
+
+let n = 100;
+let i = 0;
+
+while (i < n){
+    console.log(++i);
+}
+console.log('-'.repeat(12))
+do {
+    console.log(i--);
+} while(i >= 0);
