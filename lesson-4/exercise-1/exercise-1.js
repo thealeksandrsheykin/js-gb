340c20fb1d8cc17eee8b7e4f6d9674f17d3dff6f
thealeksandrsheykin <alexander_sheykin@.mail.ru>
2022-06-10 19:36:54 +0300

    Add a second solution for exercise-1.js

--- lesson-4/exercise-1/exercise-1.js
+++ lesson-4/exercise-1/exercise-1.js
@@ -8,19 +8,32 @@
 Если число превышает 999, необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
  */
 
+
+// Способ №1
 function number_to_string(number){
-    let result = {'единицы': 0,'десятки': 0, 'сотни': 0}
+    let result = {'единицы': 0,'десятки': 0, 'сотни': 0};
     if (number > 999){
-        console.log('Данное число выходит за рамки диапозона: 0-999')
+        console.log('Данное число выходит за рамки диапозона: 0-999');
         return result
     }
     else {
         for (let [i, j] of [['сотни', 100], ['десятки', 10], ['единицы', 1]]) {
-            result[i] = Math.trunc(number / j)
-            number = number % j
+            result[i] = Math.trunc(number / j);
+            number = number % j;
         }
     }
     return result
 }
 
+// Способ №2
+
+function number_to_string_2(number) {
+    return  {
+        'единицы':  number % 10,
+        'десятки': Math.trunc(((number / 10) % 10)),
+        'сотни':   Math.trunc((number / 100))
+    }
+}
+
 console.log(number_to_string(124))
+console.log(number_to_string_2(677))
