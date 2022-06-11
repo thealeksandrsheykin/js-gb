96b52c3f1c87d98c15958bfbe811f647adb39173
Aleksandr <alexander_sheykin@mail.ru>
2022-06-11 22:58:44 +0300

    Exercise-3 Done!

--- lesson-4/exercise-3/exercise-3.js
+++ lesson-4/exercise-3/exercise-3.js
@@ -5,3 +5,22 @@
 но и для каталога. Стремиться нужно к тому, чтобы объект «Продукт» имел единую структуру для различных модулей сайта, но в
 разных местах давал возможность вызывать разные методы.
  */
+
+const product = {
+        model: 'Test Product',
+        year:  2022,
+        color: 'blue',
+        price: 100,
+        quantityProductStore: 0,
+        availableInStore: false,
+        shopGetProduct: function() {
+            this.availableInStore = true;
+        },
+        haveProductStore: function(quantity) {
+            if (this.availableInStore) {
+                this.quantityProductStore += quantity;
+             } else {
+                alert("Для начала доставьте продукт в магазин!");
+             }
+        }
+};
