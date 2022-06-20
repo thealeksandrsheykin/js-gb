e472febbe0db8b69fd7cee65120630cf4e03076d
Aleksandr <alexander_sheykin@mail.ru>
2022-06-20 23:27:00 +0300

    Create exercise-2.js

---
+++ lesson-6/exercise-2/exercise-2.js
@@ -0,0 +1,217 @@
+#!/usr/bin/env node
+
+/*
+2. * У товара может быть несколько изображений. Нужно:
+    a. Реализовать функционал показа полноразмерных картинок товара в модальном окне;
+    b. Реализовать функционал перехода между картинками внутри модального окна.
+ */
+
+const cart = {
+    items: [],
+
+    getTotalPrice() {
+        return this.items.reduce( (acc, item) => {
+            return acc + item.price * item.count;
+        }, 0 );
+    },
+
+    getCartLength() {
+        return this.items.length;
+    },
+
+    addToCart(product) {
+        const idx = this.items.findIndex(item => {
+            return item.productId === +product.id;
+        });
+        if (idx === -1){
+            this.items.push({
+                id: this.items.length === 0 ? 0 : this.items[this.items.length-1].id + 1,
+                productId: product.id,
+                name: product.name,
+                price: product.price,
+                count: 1
+            });
+        }
+        else{
+            this.items[idx].count ++;
+        }
+        this.render();
+    },
+
+    deleteFromCart(id) {
+        const indexItem = this.items.findIndex( (item) => {
+            return item.id === id;
+        } );
+        this.items.splice(indexItem, 1);
+    },
+
+    render() {
+        const cartWrapperElement = document.querySelector('.cart');
+        cartWrapperElement.innerHTML = ''
+        const cartElement = document.createElement('div');
+        const itemH2Element = document.createElement('h4');
+        if ( this.getCartLength() === 0 ) {
+            itemH2Element.textContent = 'Корзина пуста...';
+        } else {
+            itemH2Element.textContent = `В корзине: ${this.getCartLength()} товаров на сумму ${this.getTotalPrice()} рублей`;
+        }
+        cartElement.appendChild(itemH2Element);
+        cartWrapperElement.appendChild(cartElement);
+    }
+};
+
+const catalog = {
+    cart,
+    items: [
+        {
+            id: 1001,
+            name: 'Велосипед',
+            picture_min: 'images/min/bike.jpg',
+            picture_max: 'images/max/bike.jpg',
+            class_name: 'bike',
+            price: 25000,
+            count: 5,
+        },
+        {
+            id: 1002,
+            name: 'Самокат',
+            picture_min: 'images/min/scooter.jpg',
+            picture_max: 'images/max/scooter.jpg',
+            class_name: 'scooter',
+            price: 35000,
+            count: 10,
+        },
+        {
+            id: 1003,
+            name: 'Скейтборд',
+            picture_min: 'images/min/skateboard.jpg',
+            picture_max: 'images/max/skateboard.jpg',
+            class_name: 'skateboard',
+            price: 5000,
+            count: 2,
+        },
+    ],
+
+    settings: {
+        galleryMainContainer: null,
+        modalImageContainer: 'gallery__modal',
+        modalImageClass: 'gallery__image',
+        modalImageScreen: 'gallery__back',
+        modalImageClose: 'gallery__close',
+        modalImageCloseSrc: 'images/close.png',
+    },
+
+
+    buttonClickHandler (event) {
+        if (event.target.tagName === 'BUTTON'){
+            const idx = this.items.findIndex(item => {
+                return item.id === +event.target.dataset.id;    // Разобраться как работает +
+            });
+            cart.addToCart(this.items[idx]);
+        }
+    },
+
+    pictureClickHandler (event) {
+        this.settings.galleryMainContainer = `.${event.target.parentElement.className}`;
+        if (event.target.tagName === 'IMG') {
+            this.createModal(event.target);
+        }
+        else{
+            return;
+        };
+    },
+//----------------------------------------------------------------
+
+
+
+    createModal(img) {
+        const galleryModal = document.createElement('div');
+        galleryModal.classList.add(this.settings.modalImageContainer);
+
+        const galleryScreen = document.createElement('div');
+        galleryScreen.classList.add(this.settings.modalImageScreen);
+        galleryModal.appendChild(galleryScreen);
+
+        const galleryClose = new Image();
+        galleryClose.classList.add(this.settings.modalImageClose);
+        galleryClose.src = this.settings.modalImageCloseSrc;
+        galleryClose.addEventListener('click', (event) => {
+            this.close(event.target);
+        });
+        galleryModal.appendChild(galleryClose);
+
+        const galleryImage = new Image();
+        galleryImage.classList.add(this.settings.modalImageClass);
+        galleryImage.src = img.dataset.fullImageUrl;
+        galleryModal.appendChild(galleryImage);
+
+        document.querySelector(this.settings.galleryMainContainer).appendChild(galleryModal);
+    },
+
+    close(closeImg) {
+        closeImg.parentElement.remove();
+    },
+
+//---------------------------------------------------------------------------
+    render() {
+        const catalogWrapperElement = document.querySelector('.catalog');
+        const catalogElement = document.createElement('ol');
+        this.items.forEach( item => {
+            const itemElement = document.createElement('li');
+
+            const containerElement = document.createElement('div');
+            containerElement.setAttribute('class',`${item.class_name}`);
+            itemElement.appendChild(containerElement);
+
+            // Заголовок (название продукта)
+            const itemH3Element = document.createElement('h3');
+            itemH3Element.textContent = item.name;
+            containerElement.appendChild(itemH3Element);
+
+            // Картинка
+            const itemImgElement = document.createElement('img');
+            itemImgElement.setAttribute('src', `${item.picture_min}`);
+            //itemImgElement.setAttribute('data-full-image-url',`${item.picture_max}`);
+            itemImgElement.setAttribute('alt',`${item.name}`);
+            itemImgElement.dataset.fullImageUrl = `${item.picture_max}`;
+            containerElement.appendChild(itemImgElement);
+
+
+
+            //Параграф (цена продукта)
+            const itemPElement = document.createElement('p');
+            itemPElement.innerHTML = `Цена: ${item.price} руб.`;
+            containerElement.appendChild(itemPElement);
+
+            // Кнопка купить
+            const itemBElement = document.createElement('button');
+            itemBElement.innerHTML = 'Купить';
+            itemBElement.dataset.id = item.id;
+            containerElement.appendChild(itemBElement);
+
+
+            catalogElement.appendChild(itemElement);
+
+        } );
+
+        catalogElement.addEventListener('click', (event)=>{
+            this.buttonClickHandler(event);
+        });
+
+        catalogElement.addEventListener('click', (event)=>{
+            this.pictureClickHandler(event);
+        });
+
+        catalogWrapperElement.appendChild(catalogElement);
+
+        const itemHrElement = document.createElement('hr');
+        catalogWrapperElement.appendChild(itemHrElement);
+    }
+}
+
+//-------------------------------------------------
+
+window.addEventListener('load', () => {
+    catalog.render();
+    cart.render();
+});
